225ba3c19c9a3e10eb51c479d0471c32a5521324
knightc0de <its_puneet143@proton.me>
2026-07-10 21:31:55 +0530

    working on upload panel

---
+++ labs/uploads/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000;
+
+// server 
+app.use("/uploads",express.static(path.join(__dirname,"uploads")));
+
+const storage = multer.diskstorage({
+                
+
+
+})
+
+
